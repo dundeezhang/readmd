55247d4a240b9f185c81f00f35fbf089c92dec7a
dundeezhang <dundee_zhang@hotmail.com>
2025-01-08 22:40:43 -0500

    add feature for multiple files and table of contents

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,22 @@ import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import "./App.css";
 
 function App() {
-  const [fileContent, setFileContent] = useState("");
+  const [fileContents, setFileContents] = useState([]);
 
-  const handleFileUpload = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      setFileContent(e.target.result);
-    };
-    reader.readAsText(file);
+  const handleMultipleFileUpload = (event) => {
+    const files = event.target.files;
+    let combinedContents = [];
+
+    Array.from(files).forEach((file, index) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        combinedContents.push({ name: file.name, content: e.target.result });
+        if (index === files.length - 1) {
+          setFileContents(combinedContents);
+        }
+      };
+      reader.readAsText(file);
+    });
   };
 
   return (
@@ -24,45 +31,63 @@ function App() {
           <h1 className="prog-title">Markdown File Viewer</h1>
           <p className="by-tag">by dhz</p>
           <p>
-            This is a simple markdown file viewer. Upload a markdown file and
-            see the content displayed here. The file will be read and displayed
-            in real-time.
+            This is a simple and lightweight markdown file viewer. Upload
+            markdown files and see the content displayed here. The files will be
+            read and displayed in real-time. See the table of contents for quick
+            navigation.
           </p>
+          <p>You can upload multiple files to view at the same time.</p>
         </div>
-        <h2>Submit Markdown File</h2>
+        <h2>Submit Markdown Files</h2>
         <input
           type="file"
           className="submit"
           accept=".md"
-          onChange={handleFileUpload}
+          multiple
+          onChange={handleMultipleFileUpload}
         />
+        <nav className="file-nav">
+          <ul>
+            <p>Table of Contents:</p>
+            {fileContents.map((file, index) => (
+              <li key={index}>
+                <a href={`#${file.name}`}>{file.name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
         <div className="app-content">
-          <h2>File Content:</h2>
-          <ReactMarkdown
-            components={{
-              code({ className, children, ...rest }) {
-                const match = /language-(\w+)/.exec(className || "");
-                return match ? (
-                  <SyntaxHighlighter
-                    PreTag="div"
-                    language={match[1]}
-                    style={oneDark}
-                    {...rest}
-                  >
-                    {children}
-                  </SyntaxHighlighter>
-                ) : (
-                  <code {...rest} className={className}>
-                    {children}
-                  </code>
-                );
-              },
-            }}
-          >
-            {fileContent}
-          </ReactMarkdown>
-          <hr></hr>
-          <h3 className="endoffile">End of File</h3>
+          {fileContents.map((file, index) => (
+            <div key={index} id={file.name}>
+              <hr />
+              <h3 className="filename">{file.name}</h3>
+              <ReactMarkdown
+                components={{
+                  code({ className, children, ...rest }) {
+                    const match = /language-(\w+)/.exec(className || "");
+                    return match ? (
+                      <SyntaxHighlighter
+                        PreTag="div"
+                        language={match[1]}
+                        style={oneDark}
+                        {...rest}
+                      >
+                        {children}
+                      </SyntaxHighlighter>
+                    ) : (
+                      <code {...rest} className={className}>
+                        {children}
+                      </code>
+                    );
+                  },
+                }}
+              >
+                {file.content}
+              </ReactMarkdown>
+            </div>
+          ))}
+          <hr />
+          <h3 className="endoffile">End of File(s)</h3>
           <p>
             Check out my other projects and programs at{" "}
             <a href="https://dundeezhang.com">dundeezhang.com</a>{" "}
